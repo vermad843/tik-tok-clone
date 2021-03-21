9bf0942ec21f42c39f8b3d3ec5bb0fd3f43f8996
vermad843 <vishnuverma7524@gmail.com>
2021-03-21 09:55:05 +0530

    VideoFooter

--- src/Video.js
+++ src/Video.js
@@ -1,22 +1,34 @@
-import React,{useRef} from 'react';
+import React,{useRef, useState} from 'react';
 import './Video.css';
+import VideoFooter from './VideoFooter'; 
 
-function Video() { 
+function Video() {
+    const[playing, setPlaying] = useState(false); 
     const videoRef = useRef(null);
     
+    const onVideoPress = () => {
+       if(playing) {
+         videoRef.current.pause();
+         setPlaying(false);    
+       }else {
+         videoRef.current.play();
+         setPlaying(true);    
+       }
+    };  
  
     return (
         <div className = "video">
-          
             <video
                className = "video__player"
                loop
+               onClick = {onVideoPress}
                src = "snow.mp4"
                width="560" 
                height="315"
                ref = {videoRef} 
                >
             </video>
+            <VideoFooter/>
             {/* <VideoFooter/> */}
             {/* <VideoSidebar/> */}
         </div>
---
+++ src/VideoFooter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function VideoFooter() {
+    return (
+        <div>
+            <h2>I am a Footer</h2>
+        </div>
+    )
+}
+
+export default VideoFooter
