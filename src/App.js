14f5c3e6b85716100f0e89af4473f7f338ba8774
vermad843 <vishnuverma7524@gmail.com>
2021-03-21 07:41:32 +0530

    Video component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Video from './Video';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
         <h1>Hello World</h1>
+        <div className = "app__videos">
+           <Video/>
+           <Video/>
+           <Video/>
+           <Video/>
+        </div>
     </div>
   );
 }
