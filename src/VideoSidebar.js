1929b9e93fd961e579add94e2fdd429d76ad7630
vermad843 <vishnuverma7524@gmail.com>
2021-03-24 04:19:29 +0530

    sidebar icons

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './VideoSidebar.css';
+// import FavoriteIcon from '@material-ui/icons/FavoriteIcon';
+import ShareIcon from '@material-ui/icons/Share';
+import MessageIcon from '@material-ui/icons/Message';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
 
 function VideoSidebar() {
     return (
         <div className = "videoSidebar">
-            <h2>SideBar</h2>
+           <div className = "videoSidebar__button">
+               <FavoriteBorderIcon/>
+               <p>300</p>
+           </div>
+           <div className = "videoSidebar__button">
+               <MessageIcon/>
+               <p>300</p>
+           </div> 
+           <div className = "videoSidebar__button">
+               <ShareIcon/>
+               <p>300</p>
+           </div>
         </div>
     )
 }
