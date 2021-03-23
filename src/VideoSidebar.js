5c11e441adb1df227ebcc167c6fe73290d0eff41
vermad843 <vishnuverma7524@gmail.com>
2021-03-24 04:30:27 +0530

    heart functionality

--- src/VideoSidebar.js
+++ src/VideoSidebar.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './VideoSidebar.css';
-// import FavoriteIcon from '@material-ui/icons/FavoriteIcon';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import MessageIcon from '@material-ui/icons/Message';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 
 
 function VideoSidebar() {
+    const [liked, setLiked] = useState(false);
     return (
         <div className = "videoSidebar">
            <div className = "videoSidebar__button">
-               <FavoriteBorderIcon/>
+               {liked ? 
+                  <FavoriteIcon
+                     onClick = {e => setLiked(false)}
+                  />
+                    : 
+                  <FavoriteBorderIcon
+                     onClick = {e => setLiked(true)}                  
+               />}
                <p>300</p>
            </div>
            <div className = "videoSidebar__button">
                <MessageIcon/>
-               <p>300</p>
+               <p>230</p>
            </div> 
            <div className = "videoSidebar__button">
                <ShareIcon/>
-               <p>300</p>
+               <p>10</p>
            </div>
         </div>
     )
