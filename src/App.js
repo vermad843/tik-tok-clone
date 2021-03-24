6eca0b461404c9f595e59b8c708e8b6c80d7593e
vermad843 <vishnuverma7524@gmail.com>
2021-03-24 08:32:33 +0530

    data coming from db working

--- src/App.js
+++ src/App.js
@@ -7,36 +7,27 @@ function App() {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-     db.collection('videos').onSnapshot(snapshot => {
-       setVideos(snapshot.docs.map(doc => doc.data()));
-     })
+    db.collection("videos").onSnapshot((snapshot) =>
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    );
   }, []);
 
   return (
     <div className="app">
-         <div className = "app__videos">
-             {
-               videos.map(
-                 ({
-                  channel,
-                  description,
-                  song,
-                  likes,
-                  messages,
-                  shares,
-                 },
-                  <Video
-                     channel = {channel}
-                     description = {description}
-                     song = {song}
-                     likes = {likes}
-                     messages = {messages}
-                     shares = {shares}
-                  />
-                 )
-               )
-             }
-          </div>
+      <div className="app__videos">
+        {videos.map(
+          ({ channel, description, song, likes, messages, shares }) => (
+            <Video
+              channel={channel}
+              song={song}
+              likes={likes}
+              messages={messages}
+              description={description}
+              shares={shares}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 }
