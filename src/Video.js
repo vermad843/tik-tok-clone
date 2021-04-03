f074fd6af291d8d457e0ec488ecbd28c590c3a43
vermad843 <vishnuverma7524@gmail.com>
2021-04-03 14:32:41 +0530

    change

--- src/Video.js
+++ src/Video.js
@@ -51,42 +51,4 @@ function Video({
 }
 
 export default Video;
-
-
-
-   {/* <iframe
-               className = "video__player" 
-            //    width="560" 
-            //    height="315" 
-               loop
-               controls
-               src="https://www.youtube.com/embed/8fdqUGydoPA" 
-               title="YouTube video player" 
-               frameborder="0" 
-               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-               allowfullscreen>
-            </iframe> */}
-            {/* <iframe
-               className = "video__player"  
-            //    width="560" 
-            //    height="315" 
-               src="https://www.youtube.com/embed/8fdqUGydoPA" 
-               title="YouTube video player" 
-               frameborder="0" 
-               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-               allowfullscreen>
-            </iframe> */}
-
-//  gif
-   
-              {/* <iframe 
-               className="video__player" 
-               loop
-            //    onClick = {onVideoPress}
-               ref = {videoRef} 
-               src="https://giphy.com/embed/1dLQC6YCDvelfnA3k9" 
-            //    frameBorder="0" 
-            //    allowFullScreen
-               >
-            </iframe> */}
     
